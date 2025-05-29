6c1d00a3b8c229c40e8d3fdfa6276df68f99a7ca
rSanches35 <rafaelsanches.s35@gmail.com>
2025-05-29 14:11:53 -0300

    Checkpoint CRUD com Forms

--- records/urls.py
+++ records/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
+from .views import StateCreate, CityCreate
+from .views import StateUpdate
+
 urlpatterns = [
     
+    path('create/state', StateCreate.as_view(), name='records-CreateState'),
+    path('update/state/<int:pk>', StateUpdate.as_view(), name='records-UpdateState'),
+
+    path('create/city', CityCreate.as_view(), name='records-CreateCity'),
 ]
--- records/views.py
+++ records/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView
 
-# Create your views here.
+from django.urls import reverse_lazy
+from .models import State, City, Address, Client, Relic, Adoption, AdoptionRelic
+
+class StateCreate(CreateView):
+    model = State
+    fields = ['name', 'uf']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('pages-HomePage')
+
+class StateUpdate(UpdateView):
+    model = State
+    fields = ['name', 'uf']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('pages-HomePage')
+
+    
+
+
+class CityCreate(CreateView):
+    model = City
+    fields = ['name', 'state']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('pages-HomePage')
+
