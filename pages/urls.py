7b799c91298e5b41a3a0cc058798326d5b626609
rSanches35 <rafaelsanches.s35@gmail.com>
2025-08-28 17:26:11 -0300

    Create usuário

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView, AboutView
+from .views import IndexView, AboutView, SignUpView
 
 urlpatterns = [
     
     path('', IndexView.as_view(), name='pages-HomePage'),
     path('about/', AboutView.as_view(), name='pages-AboutPage'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -8,3 +14,15 @@ class IndexView(TemplateView):
 class AboutView(TemplateView):
     
     template_name = "pages/about.html"
+
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Login automático após cadastro
+        login(self.request, self.object)
+        return response
